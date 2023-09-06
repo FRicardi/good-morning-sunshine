6f31136c7690438b91ec86fd15d01690925d03ce
felipe ricardi <felipericardi5@gmail.com>
2023-09-06 00:18:54 -0300

    feat: generating GPT completion

---
+++ src/routes/+page.server.ts
@@ -0,0 +1,20 @@
+import { GPT_API_KEY } from '$env/static/private';
+import { OpenAI } from 'openai';
+const openai = new OpenAI({apiKey: GPT_API_KEY});
+
+/** @type {import('./$types').Actions} */
+export const actions = {
+    default: async () => {
+        const completion = await openai.chat.completions.create({
+            "model": "gpt-3.5-turbo",
+            "messages": [{ "role": "user", "content": "Gere uma mensagem de positividade e motivação com no máximo 50 caracteres, desejando um bom dia." }],
+            "temperature": 1,
+            "max_tokens": 150,
+            "top_p": 1,
+            "frequency_penalty": 0.5,
+            "presence_penalty": 0.7
+        });
+    
+        return {completion: completion.choices[0].message.content}
+    }
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,43 @@
-<div class="navbar bg-base-100">
-    Good morning, sunshine!
+<script>
+    import { enhance } from '$app/forms';
+
+    let generationLoading = false, hasGenerated = false;
+    /** @type {import('./$types').ActionData} */
+    export let form;
+</script>
+<div class="navbar bg-base-100">Good morning, sunshine!</div>
+
+<div class="hero min-h-screen bg-base-200">
+	<div class="hero-content flex-col lg:flex-row-reverse">
+        {#if !hasGenerated}
+            <div class="artboard artboard-demo phone-1">
+                {#if generationLoading}
+                    <span class="loading loading-ring loading-lg"></span>
+                {:else}
+                    <p>Your image will appear here</p>
+                {/if}
+            </div>
+        {/if}
+		<div>
+
+			<h1 class="text-5xl font-bold">Let's generate a cool Good Morning Image</h1>
+            <br>
+            <form method="POST"  use:enhance={() => {
+                generationLoading = true;
+                return async ({ update }) => {
+                    generationLoading = false;
+                    update();
+                };
+            }}>
+                <input type="text" placeholder="Insert the promp to generate the image" class="input input-bordered w-full max-w-xs" />
+                <br>
+                <br>
+                <button class="btn btn-primary">Generate!</button>
+            </form>
+            {#if form?.completion}
+                <p>{form.completion}</p>
+            {/if}
+		</div>
+	</div>
 </div>
-
-
 
