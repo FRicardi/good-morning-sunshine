c1e2c2cd4263576cb0441b5963f1a69796f62e58
felipe ricardi <felipericardi5@gmail.com>
2023-09-06 23:37:19 -0300

    feat: creating script to generate image

---
+++ src/lib/imagineGeneration.js
@@ -0,0 +1,25 @@
+import { writeFileSync } from 'fs';
+import { IMAGINE_API_KEY } from "$env/static/private";
+
+const generateImage = async () => {
+    const formData = new FormData();
+    formData.append("prompt", "A happy dog and a cute bunny in a strawberry field");
+    formData.append("style_id", "31");
+    formData.append("steps", "40");
+    formData.append("cfg", "4.5");
+    
+    const generateImageFetch = await fetch("https://api.vyro.ai/v1/imagine/api/generations", {
+        headers: {
+            "Authorization": `Bearer ${IMAGINE_API_KEY}`
+        },
+        body: formData,
+        method: "POST"
+    });
+
+    const generatedImageBlob = await generateImageFetch.blob();
+    
+    writeFileSync(`static/generated-image.png`, Buffer.from(await
+        generatedImageBlob.arrayBuffer()));
+
+
+}
