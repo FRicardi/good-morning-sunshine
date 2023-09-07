4408c294f5cc70f45c160dad2282443276ba75dd
felipe ricardi <felipericardi5@gmail.com>
2023-09-06 23:10:16 -0300

    fix: improving landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,6 +2,7 @@
     import { enhance } from '$app/forms';
 
     let generationLoading = false, hasGenerated = false;
+
     /** @type {import('./$types').ActionData} */
     export let form;
 </script>
@@ -10,22 +11,26 @@
 <div class="hero min-h-screen bg-base-200">
 	<div class="hero-content flex-col lg:flex-row-reverse">
         {#if !hasGenerated}
-            <div class="artboard artboard-demo phone-1">
+            <div class="artboard artboard-demo square">
                 {#if generationLoading}
                     <span class="loading loading-ring loading-lg"></span>
                 {:else}
                     <p>Your image will appear here</p>
                 {/if}
             </div>
+        {:else}
+            <img src="/generated-image.png" alt="AI response" width="520" height="520">
         {/if}
 		<div>
 
 			<h1 class="text-5xl font-bold">Let's generate a cool Good Morning Image</h1>
             <br>
-            <form method="POST"  use:enhance={() => {
+            <form method="POST" use:enhance={() => {
                 generationLoading = true;
                 return async ({ update }) => {
                     generationLoading = false;
+                    hasGenerated = true;
+                    
                     update();
                 };
             }}>
@@ -34,9 +39,6 @@
                 <br>
                 <button class="btn btn-primary">Generate!</button>
             </form>
-            {#if form?.completion}
-                <p>{form.completion}</p>
-            {/if}
 		</div>
 	</div>
 </div>
