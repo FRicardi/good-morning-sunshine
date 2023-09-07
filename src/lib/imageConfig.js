890939d48360dc7d9b1df8882dbd6d7be0a1ff4c
felipe ricardi <felipericardi5@gmail.com>
2023-09-07 00:13:19 -0300

    feat: updating text overlay method

---
+++ src/lib/imageConfig.js
@@ -0,0 +1,9 @@
+export default {
+    imageWidth: 520,
+    imageHeight: 520,
+    textOffset: 70,
+    textDisplacement: {
+        x: 5,
+        y: 445
+    }
+}
--- src/lib/overlayTextOnImage.js
+++ src/lib/overlayTextOnImage.js
@@ -1,21 +1,68 @@
 import path from 'path';
 import { fileURLToPath } from 'url';
 import Jimp from "jimp";
+import imageConfig from "./imageConfig";
 
 export default async (
-        /** @type {string} */ 
-        text, 
-        /** @type {string} */ 
-        imagePath
-    ) => {
+    /** @type {string} */
+    text,
+    /** @type {string} */
+    imagePath
+) => {
+    const { image, font } = await loadImageAndFont(imagePath);
+
+    const backgroundTextImage = await generateBackgroundTextImage(text, font);
+
+    image
+        .resize(imageConfig.imageWidth, imageConfig.imageHeight)
+        .blit(backgroundTextImage, 0, 0)
+        .print(font,
+            imageConfig.textDisplacement.x,
+            imageConfig.textDisplacement.y,
+            {
+                text,
+                alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+            },
+            imageConfig.imageWidth,
+            imageConfig.imageHeight);
+
+    await image.writeAsync("static/generated-image-with-text.png");
+}
+
+const generateBackgroundTextImage = async (
+    /** @type {string} */
+    text,
+    /** @type {import("@jimp/plugin-print").Font} */
+    font
+) => {
+    let textImage = new Jimp(imageConfig.imageWidth, imageConfig.imageHeight, 0x0);
+    textImage.print(font,
+        0,
+        imageConfig.imageHeight - imageConfig.textOffset,
+        {
+            text,
+            alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        },
+        imageConfig.imageWidth,
+        imageConfig.imageHeight);
+
+    // @ts-ignore
+    textImage.color([{ apply: 'xor', params: ["yellow"] }]);
+
+    return textImage;
+}
+
+const loadImageAndFont = async (
+    /** @type {string} */
+    imagePath
+) => {
     const __filename = fileURLToPath(import.meta.url);
     const __dirname = path.dirname(__filename);
     const image = await Jimp.read(path.join(__dirname, `../../static/${imagePath}`));
     const font = await Jimp.loadFont(path.join(__dirname, "../../static/LuckiestGuyFont.fnt"));
 
-    image.print(font, 20, 30, text);
-
-    await image.writeAsync("static/generated-image-with-text.png");
+    return {
+        image,
+        font
+    }
 }
-
-const load
